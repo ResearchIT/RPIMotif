f11f9f3fd5260979866953e963ceeb6b3da17a96
Nick Booher <njbooher@iastate.edu>
2021-06-29 17:15:45 -0500

    add run_me.sh

---
+++ run_me.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rna_sequence_file=$1
+protein_sequence_file=$2
+pipeline=$3
+result_file=$4
+output_prediction_file=$5
+arff_preprocess_file=$6
+arff_intermediate_file=$7
+
+/opt/app-root/src/format_converter.pl $rna_sequence_file $protein_sequence_file $pipeline $arff_preprocess_file
+/opt/app-root/src/createARFF_test.pl --input $arff_preprocess_file --output $arff_intermediate_file
+/opt/app-root/src/runModelonTest.pl --input $arff_intermediate_file --output $output_prediction_file --outstep2 $result_file
